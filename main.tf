8876ea8f88ffac69a6b02261feba68e11a04d3ec
ankur51206 <32640688+ankur51206@users.noreply.github.com>
2022-12-21 21:59:29 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,13 +3,13 @@ provider "azurerm" {
   features {}
 }
 
-data "azurerm_management_group" "baringsroot" {
+data "azurerm_management_group" "myroot" {
   display_name = "ankur management group"
 }
 
 # My Provider finish
 
-data "azurerm_user_assigned_identity" "mi-cloudops-azpolicy" {
+data "azurerm_user_assigned_identity" "mycloudazpolicy" {
   name                = "MyIdentity"
   resource_group_name = "sample-1"
 }
@@ -402,7 +402,7 @@ resource "azurerm_subscription_policy_assignment" "assign_policy" {
 
   identity {
     type         = "UserAssigned"
-    identity_ids = [data.azurerm_user_assigned_identity.mi-cloudops-azpolicy.id]
+    identity_ids = [data.azurerm_user_assigned_identity.mycloudazpolicy.id]
 
   }
 
